5b8118bb29d0f641acfa473b9081158e80bac1e0
George <gnjuki19@gmail.com>
2023-05-26 16:39:10 +0300

    Able to render the cart component and items added to cart. Also able to remove them.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, Col, Form, Image, ListGroup, Row } from "react-bootstrap";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
+import {AiFillDelete} from 'react-icons/ai'
 
 function Cart() {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
+  const [total, setTotal] = useState();
+
+  useEffect(() => {
+    setTotal(
+      cart.reduce((acc, curr) => acc + Number(curr.price) * curr.qty, 0)
+    );
+  }, [cart]);
+
   return (
-    <div>Cart</div>
-  )
+    <div className="home">
+      <div className="productContainer">
+        <ListGroup>
+          {cart.map((product) => {
+            return (
+              <ListGroup.Item key={product.id}>
+                <Row>
+                  <Col md={2}>
+                    <Image
+                      src={product.image}
+                      alt={product.name}
+                      fluid
+                      rounded
+                    />
+                  </Col>
+                  <Col md={2}>
+                    <span>{product.name}</span>
+                  </Col>
+                  <Col md={2}>{product.price}</Col>
+                  <Col md={2}>
+                    <Rating rating={product.ratings} />
+                  </Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={product.qty}
+                      onChange={(e) =>
+                        dispatch({
+                          type: "CHANGE_CART_QTY",
+                          payload: {
+                            id: product.id,
+                            qty: e.target.value,
+                          },
+                        })
+                      }
+                    >
+                      {[...Array(product.inStock).keys()].map((x) => (
+                        <option key={x + 1}>{x + 1}</option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() =>
+                        dispatch({
+                          type: "REMOVE_FROM_CART",
+                          payload: product,
+                        })
+                      }
+                    >
+                      <AiFillDelete fontSize="20px" />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+      </div>
+      <div className="filters summary">
+        <span className="title">You have ({cart.length}) items in cart.</span>
+        <span style={{ fontWeight: 700, fontSize: 20 }}>
+          Total: KES {total}
+        </span>
+        <Button type="button" variant="primary" disabled={cart.length === 0}>
+          Proceed to Checkout
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
